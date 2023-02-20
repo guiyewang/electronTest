efd4d5dfa9769f0bfb3e6bf67f3b6704fd64a260
OfficePC <455045618@qq.com>
2023-02-20 17:29:54 +0800

    修改说明

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 软件版本
 Ndoejs 18.14.1
 Electron ^23.1.0
+
+UI组件库
+Naive UI https://www.naiveui.com/zh-CN
--- electron/index.ts
+++ electron/index.ts
@@ -12,7 +12,8 @@ const createWindow = () => {
         webPreferences: {
             devTools: true,
             contextIsolation: false,
-            nodeIntegration: true
+            nodeIntegration: true,
+           
             //允许html页面上的javascipt代码访问nodejs 环境api代码的能力（与node集成的意思）
         }
     })
